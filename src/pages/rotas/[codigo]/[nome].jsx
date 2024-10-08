5b2ff5c4db8decd6062027c65f30850cf2a49fcd
naiaragabriela <naigabi31@gmail.com>
2024-10-08 08:50:23 -0300

    Feat: Create Routes

---
+++ src/pages/rotas/[codigo]/[nome].jsx
@@ -0,0 +1,15 @@
+import { useRouter } from 'next/router';
+
+export default function codigoNome() {
+
+    
+    const router = useRouter()
+    const codigo = router.query.codigo
+    const nome = router.query.nome
+
+    return (
+        <div>
+            <h1>Rotas / {codigo} / {nome} </h1>
+        </div>
+    )
+}
